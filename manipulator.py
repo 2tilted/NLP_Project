c70afe805b83e4a367035ad1eaffda9a1ee3fc7d
tiltedtony <tutoni.nguyentuan@stud.tu-darmstadt.de>
2019-12-16 14:22:18 +0100

    added acl latex template

--- manipulator.py
+++ manipulator.py
@@ -15,6 +15,7 @@ prediction = classifier.classify(test_text, './polarity_classifier.sav')
 test_text_pos = []
 test_text_neg = []
 
+"""filter between positive and negative"""
 for pol in range(len(prediction)):
     if prediction[pol] == 3:
         test_text_pos.append(test_text[pol])
@@ -30,6 +31,7 @@ def create_tf_idf_matrix(documents):
 
     return sorted_scores
 
+"""create tf idf matrices and filter out the non-adjectives"""
 sorted_scores_pos = create_tf_idf_matrix(test_text_pos)
 #sorted_scores_pos = [i[0] for i in sorted_scores_pos]
 sorted_scores_pos = [i[0] for i in sorted_scores_pos if nlp(i[0])[0].pos_ == 'ADJ']
@@ -38,6 +40,7 @@ sorted_scores_neg = create_tf_idf_matrix(test_text_neg)
 #sorted_scores_neg = [i[0] for i in sorted_scores_neg]
 sorted_scores_neg = [i[0] for i in sorted_scores_neg if nlp(i[0])[0].pos_ == 'ADJ']
 
+"""remove identical adjectives"""
 for x in sorted_scores_pos[:]:
     if x in sorted_scores_neg:
         sorted_scores_neg.remove(x)
@@ -52,7 +55,7 @@ for x in sorted_scores_pos[:]:
 
 print(sorted_scores_neg)
 
-
+"""create new csv file with modified text"""
 df = pd.read_csv('./evaluation_examples.csv', sep=",", header=None)
 for sentence in range(len(df[0])):
     for word in df[0][sentence].split():
