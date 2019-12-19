b17769852c5ed70cf0cfe74da530ed3622da74de
Constantin <cns.stp@gmx.de>
2019-12-19 13:02:11 +0100

    use preprocessor and tokenizer of vectorizer instead of split

--- manipulator.py
+++ manipulator.py
@@ -1,8 +1,10 @@
 import classifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.utils.sparsefuncs import csc_median_axis_0
 import numpy as np
 import pandas as pd
 import spacy
+import pickle
 import itertools
 
 nlp = spacy.load("en_core_web_sm")
@@ -19,18 +21,19 @@ test_text_1_3 = []
 test_text_0_4 = []
 test_text_1_4 = []
 
-for pol in range(len(polarity_prediction)):
-    if domain_prediction[pol] == 0 and polarity_prediction[pol] == 3:
-        test_text_0_3.append(test_text[pol])
-    elif domain_prediction[pol] == 1 and polarity_prediction[pol] == 3:
-        test_text_1_3.append(test_text[pol])
-    elif domain_prediction[pol] == 0 and polarity_prediction[pol] == 4:
-        test_text_0_4.append(test_text[pol])
+for idx in range(len(polarity_prediction)):
+    if domain_prediction[idx] == 0 and polarity_prediction[idx] == 3:
+        test_text_0_3.append(test_text[idx])
+    elif domain_prediction[idx] == 1 and polarity_prediction[idx] == 3:
+        test_text_1_3.append(test_text[idx])
+    elif domain_prediction[idx] == 0 and polarity_prediction[idx] == 4:
+        test_text_0_4.append(test_text[idx])
     else:
-        test_text_1_4.append(test_text[pol])
+        test_text_1_4.append(test_text[idx])
 
+
+vectorizer = TfidfVectorizer(ngram_range=(1,2))
 def create_tf_idf_matrix(documents):
-    vectorizer = TfidfVectorizer(ngram_range=(1,3))
     tf_idf_matrix = vectorizer.fit_transform(documents)
     scores = zip(vectorizer.get_feature_names(),
                      np.asarray(tf_idf_matrix.sum(axis=0)).ravel())
@@ -54,11 +57,11 @@ def filter_polarity(pos_list, neg_list):
         tmp = [item for item in pos_list if item[0] == word[0]]
         if(len(tmp) == 1):
             q = tmp[0][1]/word[1]
-            if(q < .75):
+            if(q < .75): #the ngram has negative polarity
                 pos_list.remove(tmp[0])
-            elif(q > 1.25):
+            elif(q > 1.25): #the ngram has positive polarity
                 neg_list.remove(word)
-            else:
+            else: #the ngram is neutral
                 pos_list.remove(tmp[0])
                 neg_list.remove(word)
 
@@ -77,7 +80,6 @@ filter_polarity(sorted_scores_1_3, sorted_scores_1_4)
     #        sorted_scores_pos.remove(ngram)
      #       break
 
-"""
 print("electronic positives:")
 print(sorted_scores_0_3)
 print("\nkitchen positives:")
@@ -86,36 +88,48 @@ print("\nelectronic negatives:")
 print(sorted_scores_0_4)
 print("\nkitchen negatives:")
 print(sorted_scores_1_4)
-"""
 
 sorted_scores_0_3 = [i[0] for i in sorted_scores_0_3]
 sorted_scores_1_3 = [i[0] for i in sorted_scores_1_3]
 sorted_scores_0_4 = [i[0] for i in sorted_scores_0_4]
 sorted_scores_1_4 = [i[0] for i in sorted_scores_1_4]
 
-df = pd.read_csv('./evaluation_examples.csv', sep=",", header=None)
-for sentence in range(len(df[0])):
-    for word in df[0][sentence].split():
+df = test_data
+
+vectorizer = TfidfVectorizer()
+analyzer = vectorizer.build_analyzer()
+preprocessor = vectorizer.build_preprocessor()
+
+for sentence_idx in range(len(df[0])):
+    for word in df[0][sentence_idx].split():
         if word in sorted_scores_0_3:
-            s = df.at[sentence, 0]
-            s = s.replace(word,sorted_scores_0_4[0])
-            print(s)
-            df.at[sentence, 0] = s
+            s = df.at[sentence_idx, 0]
+            s = s.replace(word, sorted_scores_0_4[0])
+            if len(word.split()) > 1:
+                print("BIGRAM \n")
+            #print(s)
+            df.at[sentence_idx, 0] = s
         elif word in sorted_scores_0_4:
-            s = df.at[sentence, 0]
-            s = s.replace(word,sorted_scores_0_3[0])
-            print(s)
-            df.at[sentence, 0] = s
+            s = df.at[sentence_idx, 0]
+            s = s.replace(word, sorted_scores_0_3[0])
+            if len(word.split()) > 1:
+                print("BIGRAM \n")
+            #print(s)
+            df.at[sentence_idx, 0] = s
         elif word in sorted_scores_1_3:
-            s = df.at[sentence, 0]
+            s = df.at[sentence_idx, 0]
             s = s.replace(word, sorted_scores_1_4[0])
-            print(s)
-            df.at[sentence, 0] = s
+            if len(word.split()) > 1:
+                print("BIGRAM \n")
+            #print(s)
+            df.at[sentence_idx, 0] = s
         elif word in sorted_scores_1_4:
-            s = df.at[sentence, 0]
-            s = s.replace(word, sorted_scores_1_4[0])
-            print(s)
-            df.at[sentence, 0] = s
-        else:
-            print("nothing found")
+            s = df.at[sentence_idx, 0]
+            s = s.replace(word, sorted_scores_1_3[0])
+            if len(word.split()) > 1:
+                print("BIGRAM \n")
+            #print(s)
+            df.at[sentence_idx, 0] = s
+        #else:
+            #print("nothing found")
 df.to_csv('copy_of_' + 'evaluation_examples.csv', index=False, header=None)
